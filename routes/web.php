bc45f7c0c6227195031398c9a371af315d7c4f17
amra24-cellra <pakarang.boon@vru.ac.th>
2026-09-25 09:41:37 +0700

    Fix duplicate Route import

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@ use Illuminate\Support\Facades\Storage;
 use App\Http\Controllers\LicenseController;  
 use App\Http\Controllers\UserController; 
 use App\Http\Controllers\VehicleController; 
-use Illuminate\Support\Facades\Route;
 
 Route::resource('license', LicenseController::class);
 Route::resource('user', UserController::class);
